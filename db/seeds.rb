e59f642a7b7aba0c9d5304145fe632131783a879
test rails <testrailsemail@gmail.com>
2017-07-14 15:41:52 +0300

    Framing: activeadmin setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'pierre@michaux.com', password: 'progenitor', password_confirmation: 'progenitor') if Rails.env.development?
