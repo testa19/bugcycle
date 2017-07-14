8f3da2c6e5b49e959768f8413f58d98fa0dee7cf
test rails <testrailsemail@gmail.com>
2017-07-14 18:24:57 +0300

    Framing: models relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714123250) do
+ActiveRecord::Schema.define(version: 20170714135808) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -41,6 +41,67 @@ ActiveRecord::Schema.define(version: 20170714123250) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "bicycles", force: :cascade do |t|
+    t.string   "name",        limit: 100
+    t.text     "description"
+    t.integer  "category_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["category_id"], name: "index_bicycles_on_category_id"
+    t.index ["name"], name: "index_bicycles_on_name", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 30
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "commits", force: :cascade do |t|
+    t.integer  "bicycle_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bicycle_id"], name: "index_commits_on_bicycle_id"
+    t.index ["user_id"], name: "index_commits_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "bicycle_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bicycle_id"], name: "index_likes_on_bicycle_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "pics", force: :cascade do |t|
+    t.string   "image"
+    t.string   "name"
+    t.integer  "bicycle_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bicycle_id"], name: "index_pics_on_bicycle_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
