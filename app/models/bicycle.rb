52f163d3b33b7e0cd85d7827d61a933f69e3066a
test rails <testrailsemail@gmail.com>
2017-07-14 19:50:26 +0300

    Framing: models validations

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,10 +1,14 @@
 class Bicycle < ApplicationRecord
-  belongs_to :category
+  belongs_to :category # , optional: true
   has_one :commit, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_one :pic,  dependent: :destroy
-  has_many :users, through: :likes
+  has_many :liked_by, through: :likes, source: :user
   has_one :user, through: :commit
 
-  before_save { name.downcase! }
+  validates :name, uniqueness: true, presence: true
+
+  before_save { name.downcase! } 
+  # than capitilise if needed or we can store original name in attribute 
+  # like: attr_accessor :oname... oname = name; name.downcase!
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :bicycles
+
+  validates :name, uniqueness: true, presence: true
 end
