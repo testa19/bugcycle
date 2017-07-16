1f96782abe19a4b4cba4a70423c9c0fe2fcec6e1
test rails <testrailsemail@gmail.com>
2017-07-16 13:43:45 +0300

    Sorting: with ajax

--- app/helpers/bicycles_helper.rb
+++ app/helpers/bicycles_helper.rb
@@ -1,2 +1,9 @@
 module BicyclesHelper
+  def sortable(column, name = nil)
+    name ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to name, permitted_params.merge(sort: column, direction: direction, page: nil), {class: css_class, remote: true}
+    # carries all parameters accross on link click (merges them with sort and direction params)
+  end
 end
