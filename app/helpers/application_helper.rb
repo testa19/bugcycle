97fa6eb13c170944e794cace9a9d2e99c9ac970e
test rails <testrailsemail@gmail.com>
2017-07-17 00:52:18 +0300

    Filtering: simple filter (helpers, concerns, partials)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,10 @@ module ApplicationHelper
     end
     content_tag("div", attributes, &block)
   end
-  
+  def filter_chbox_option(filter_name, filter_value)
+    id = filter_name.to_s.singularize
+    flag = (params[filter_name].include? filter_value.to_s) unless params[filter_name].nil?
+
+    check_box_tag "#{filter_name}[]", filter_value, flag, {id: id }    
+  end
 end
